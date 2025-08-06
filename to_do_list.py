e89b7575069fff14e5513e022b4ce0ee4cb03ee5
RAJAT KAPSEKAR <123821900+rajat358@users.noreply.github.com>
2025-08-06 20:38:27 +0530

    Add files via upload

---
+++ to_do_list.py
@@ -0,0 +1,23 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/HP/Desktop/to _do_list_application.py ===========
+
+===== To-Do List =====
+1. Add Task
+2. Show Tasks
+3. Mark Task as Done
+4. Exit
+Enter your choice: 1
+
+How may task you want to add: 2
+Enter the task: try id
+Task added!
+Enter the task: welcome
+Task added!
+
+===== To-Do List =====
+1. Add Task
+2. Show Tasks
+3. Mark Task as Done
+4. Exit
